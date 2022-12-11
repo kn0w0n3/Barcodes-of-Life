9c65ebed30efbdd36d39e9546e1e2f3879b4706a
equati0n <tr14rc3@gmail.com>
2022-12-10 17:52:49 -0800

    Add Search Activity |  Adjust Buttons

--- BOLDNG/app/src/main/java/com/example/bold_ng/MainController.java
+++ BOLDNG/app/src/main/java/com/example/bold_ng/MainController.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class MainController extends AppCompatActivity {
 
     Button recordDataBtn;
+    Button searchDataBtnx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +18,25 @@ public class MainController extends AppCompatActivity {
         setContentView(R.layout.activity_main_controller);
         getSupportActionBar().hide();
 
+        //Open the input data window when user clicks button
         recordDataBtn = findViewById(R.id.recordDataBtn);
-                recordDataBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent(MainController.this, RecordData.class);
-                        startActivity(intent);
-                    }
-                });
+        recordDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainController.this, RecordData.class);
+                startActivity(intent);
+            }
+        });
+
+        //Open the search data window when user clicks button
+        searchDataBtnx = findViewById(R.id.searchDataBtn);
+        searchDataBtnx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent2 = new Intent(MainController.this, SearchData.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
