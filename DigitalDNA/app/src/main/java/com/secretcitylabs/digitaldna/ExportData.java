8cfd08b7f108bf7e3a88f3a883bb5b26df03b4df
equati0n <tr14rc3@gmail.com>
2022-12-28 22:59:57 -0800

    Custom Spinner for Data  Export

--- DigitalDNA/app/src/main/java/com/secretcitylabs/digitaldna/ExportData.java
+++ DigitalDNA/app/src/main/java/com/secretcitylabs/digitaldna/ExportData.java
@@ -13,13 +13,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ExportData extends AppCompatActivity {
     DatabaseHelper exportDatabase;
     Button exportDataBtn;
     TextView dbContentsTextView;
+    TextView dbCountTextView;
     //Spinner spinner;
+
+    int databaseEntryCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +34,29 @@ public class ExportData extends AppCompatActivity {
         exportDatabase = new DatabaseHelper(this);
         exportDataBtn = (Button) findViewById(R.id.exportDataButton);
         dbContentsTextView = findViewById(R.id.scrollViewTextView);
+        dbCountTextView = findViewById(R.id.dbEntryCountTxtView);
+
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
         // Spinner Drop down elements
-        List<String> formats = new ArrayList<String>();
-        formats.add("Excel");
-        formats.add("CSV");
-        formats.add("TSV");
-        formats.add("PDF");
+        List<String> formats = Arrays.asList(" Select Format", "Excel", "CSV", "TSV", "PDF");
 
-        //https://www.tutorialspoint.com/android/android_spinner_control.htm
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  android.R.layout.simple_spinner_item, formats);
+        //https://www.youtube.com/watch?v=N8GfosWTt44
+        //Create an ArrayAdapter using custom layout
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.spinner_text_color, formats);
 
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Specify the custom layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.spinner_drop_down);
 
-        // Apply the adapter to the spinner
+        //Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        List<String> whatToExport = Arrays.asList(" Select Export Type", "Selected Items", "All");
+        ArrayAdapter adapter2 = new ArrayAdapter(getApplicationContext(), R.layout.spinner_text_color, whatToExport);
+        adapter2.setDropDownViewResource(R.layout.spinner_drop_down);
+        spinner2.setAdapter(adapter2);
 
         //Get info from database and populate the scroll view
         getDatabaseEntries();
@@ -72,8 +81,10 @@ public class ExportData extends AppCompatActivity {
         }
         StringBuffer buffer = new StringBuffer();
         while(res.moveToNext()){
+            databaseEntryCount++;
             buffer.append("id: " + res.getString(0) + "\n");
         }
+        dbCountTextView.setText(String.valueOf(databaseEntryCount));
         dbContentsTextView.append(buffer.toString());
     }
 }
